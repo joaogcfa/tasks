b79e0b5e9019b593dc2c962b2d1c34a61d2d18af
Joao Guilherme Almeida <joaogcfa@al.insper.edu.br>
2021-12-01 16:50:02 -0300

    ajustando erros

--- tasks/views.py
+++ tasks/views.py
@@ -28,17 +28,18 @@ def task_detail(request, pk):
         serializer = TaskSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            serializer_json = serializers.serialize("json", serializer)
             return HttpResponse(request.data, content_type="application/json", status=status.HTTP_201_CREATED)
         return HttpResponse(request.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         task.delete()
         serializer_json = serializers.serialize("json", task)
-        return HttpResponse(serializer_json, content_type="application/json", status=status.HTTP_20)
+        return HttpResponse(serializer_json, content_type="application/json", status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
 def get_task(request):
     get_task = Task.objects.all()
     serializer_json = serializers.serialize("json", get_task)
-    return HttpResponse(serializer_json, content_type="application/json", status=status.HTTP_201_CREATED)
+    return HttpResponse(serializer_json, content_type="application/json", status=status.HTTP_200_OK)
