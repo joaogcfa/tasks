776bdb541acb5e8e43df8be13bcbf03ceddbcb2f
Joao Guilherme Almeida <joaogcfa@al.insper.edu.br>
2021-12-01 12:02:29 -0300

    endpoints que possivelmente estejam errado

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,13 @@
-from .models import Task
+from django.shortcuts import render
+from django.http import HttpResponse
 from rest_framework import viewsets
-from .serializers import TaskSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.contrib.auth.models import Task
+from serializers import TaskSerializer
+from models import Task
 
 
 class TaskViewSet(viewsets.ModelViewSet):
@@ -13,3 +20,30 @@ class TaskViewSet(viewsets.ModelViewSet):
 
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def snippet_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        task = Task.objects.get(pk=pk)
+        tasks = Task.objects.all()
+    except Task.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = TaskSerializer(tasks)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
